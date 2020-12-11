c48d85d9ac5b3eacd47c2746b0315a227312909d
Jin Do <doyj2012@gmail.com>
2020-12-11 18:43:56 +0900

    protect credentials and update readme.md

--- APIProjectFolder/APIProject/settings.py
+++ APIProjectFolder/APIProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from credentials import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7&%vw&&_5s$4qj*i^n#h5xm@q1eteyh@*6)$!dkbwv3w^4k0$h'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DjangoRestAPIDemo
 Following blog tutorial to learn Django and RestAPI
+
+https://datagraphi.com/blog/post/2019/12/19/rest-api-guide-productionizing-a-machine-learning-model-by-creating-a-rest-api-with-python-django-and-django-rest-framework
